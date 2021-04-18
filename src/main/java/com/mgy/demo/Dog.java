51d514403946d0cbd5af174b14cbb3b3e6d2e109
mgy <mgygg3@163.com>
2021-04-18 23:22:10 +0800

    Initial commit

---
+++ src/main/java/com/mgy/demo/Dog.java
@@ -0,0 +1,40 @@
+package com.mgy.demo;
+
+/**
+ * @author mgy
+ * @date 2021/4/18 23:19
+ */
+
+public class Dog {
+    private int id;
+    private String name;
+
+    public Dog(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
